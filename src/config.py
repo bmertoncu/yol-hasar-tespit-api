7c0abb2b143d4b64e0eafc247c426b66a5f99366
Burak Mert <burakmertoncu5@gmail.com>
2026-07-07 14:36:10 +0300

    yorum satırları eklendi

--- src/config.py
+++ src/config.py
@@ -1,10 +1,15 @@
 import os
 
 class Settings:
+    # Proje İsmi
     PROJECT_NAME: str = "Belediye Araçları Canlı Yol Hasar Tespit Sistemi"
+    # Yolo Model Yolu
     MODEL_PATH: str = os.getenv("MODEL_PATH", "models/best.pt")
+    # Güven Eşiği (0,70 veya 0,71 en iyisi)
     CONFIDENCE_THRESHOLD: float = 0.70
+    # Geçici dosya yolu
     TEMP_DIR: str = "stream_temp"
+    # Log yolu
     LOG_FILE: str = "pothole_incidents.log"
     
     # Güvenlik katmanı: Araçların sunucuya yetkisiz veri atmasını engellemek için
