0475e9972275befb78eb6ecf9a06d030516ea72f
Burak Mert <burakmertoncu5@gmail.com>
2026-07-13 11:43:36 +0300

    app.js

--- frontend/app.js
+++ frontend/app.js
@@ -9,7 +9,7 @@ const ctx = canvas.getContext('2d');
 
 // Sabitler
 const CONFIDENCE_THRESHOLD = 0.71;
-const API_BASE_URL = 'https://neat-windows-guess.loca.lt'; // Kendi güncel tünel adresini buraya yaz
+const API_BASE_URL = ' https://ready-houses-sneeze.loca.lt'; // Kendi güncel tünel adresini buraya yaz
 const ADANA_BOUNDS = {
     minLat: 36.5, maxLat: 38.0,
     minLon: 34.8, maxLon: 36.2
