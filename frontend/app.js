8dafea3c799ba9ae5fdfbcc4e1a73a9505fd9638
Burak Mert <burakmertoncu5@gmail.com>
2026-07-13 10:41:46 +0300

    a

--- frontend/app.js
+++ frontend/app.js
@@ -14,7 +14,7 @@ const ctx = canvas.getContext('2d');
 // Sabitler
 const CONFIDENCE_THRESHOLD = 0.71; 
 
-const API_BASE_URL = 'https://true-carrots-repeat.loca.lt';
+const API_BASE_URL = 'https://lovely-turkeys-stick.loca.lt';
 
 // Oturum ve Güvenlik Yönetimi
 let sessionId = localStorage.getItem('sessionId') || Math.random().toString(36).substring(2, 15);
